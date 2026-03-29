81b4a3f075ece2ff0b066d6b8b8942efe22a547c
Jooaodl02 <joao.lamano@gmail.com>
2026-03-29 14:21:11 -0300

    ajuste nome csv

--- glue-jobs/glue_job_goalscorers.py
+++ glue-jobs/glue_job_goalscorers.py
@@ -5,9 +5,9 @@ from pyspark.sql import DataFrame
 from pyspark.sql.functions import col, date_format, to_date
 
 # Parâmetros explícitos (substitua por variáveis de ambiente em produção)
-JOB_NAME = "futebol_goalsscorers_job"
-SOURCE_S3_PATH = "s3://bucket-futebol/raw/goalsscorers/goalsscorers.csv"
-TARGET_S3_PATH = "s3://bucket-futebol/processed/goalsscorers/"
+JOB_NAME = "futebol_goalscorers_job"
+SOURCE_S3_PATH = "s3://bucket-futebol/raw/goalscorers/goalscorers.csv"
+TARGET_S3_PATH = "s3://bucket-futebol/processed/goalscorers/"
 INPUT_DATE_FORMAT = "yyyy-MM-dd"
 OUTPUT_DATE_FORMAT = "yyyyMMdd"
 WRITE_MODE = "overwrite"
